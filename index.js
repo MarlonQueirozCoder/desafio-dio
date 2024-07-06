725d4bf36af567d706cede9afdad4e21acb7cdcc
MarlonQueirozCoder <marlonpereira6@hotmail.com>
2024-07-05 23:09:40 -0300

    desafio-dio

--- index.js
+++ index.js
@@ -18,39 +18,39 @@ function nome(){
 }
 
 function P(F, B, P, O, D, L, I) {
-
+    let saldoV = [10, 20, 50, 80, 90, 100, 101] 
     let vitorias = 0
     for(let i = 0; i < 1; i++) {
         console.log("Parabéns ")
     
     if(vitorias === 10){        
-    console.log( F + " menor do que nível 10")
+    console.log( F + " menor do que nível 10" + saldoV[0])
 
 
     }
 
       if (vitorias > 11 && vitorias < 20){
-            console.log(B + " avançou mais um nível") 
+            console.log(B + " avançou mais um nível" + " e tem " + saldoV[0,1] + " vitórias") 
 
     }
        if (vitorias >= 21 && vitorias <= 50){
-            console.log(P + " Parabéns, a sua jornada acabou de começar")
+            console.log(P + " Parabéns, a sua jornada acabou de começar" + " e tem " + saldoV[0,2] + " vitórias")
 
     }
         if (vitorias >= 51 && vitorias <= 80){
-            console.log(O + " já pode falar para seus familiares de suas grandes conquistas")
+            console.log(O + " já pode falar para seus familiares de suas grandes conquistas" + " e tem " + saldoV[0,3] + " vitórias")
 
     }
         if (vitorias >= 81 && vitorias <= 90) {
-            console.log(D + " os diamantes são realmente brutos!")
+            console.log(D + " os diamantes são realmente brutos!" + " e tem " + saldoV[0,4] + " vitórias")
 
     }
         if (vitorias >= 91 && vitorias <= 100){
-            console.log(L + " você é uma lenda!")
+            console.log(L + " você é uma lenda!" + " e tem " + saldoV[0,5] + " vitórias")
 
     }
         else {
-            console.log(I + " imortais nunca morrem!")
+            console.log(I + " imortais nunca morrem!" + " e tem " + saldoV[0,6] + " vitórias")
     }
 }
 }
